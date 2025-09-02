891630886d03346bbe19f0cd6c5bc6bc689253c4
QuentrixHive <quentrixhive@gmail.com>
2025-09-02 21:53:06 +0000

    default is light theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,6 @@ export default function RootLayout({
         <ThemeProvider
           attribute="class"
           defaultTheme="light"
-          enableSystem
           disableTransitionOnChange
         >
             <LanguageProvider>
